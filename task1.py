368125b88e3f65f0bd9145e7111cc4252c67b91f
mershchii <mershchi@gmail.com>
2026-07-29 17:43:08 +0300

    Додані коментарі та змінена тлохи логіка з 0 та 1

--- task1.py
+++ task1.py
@@ -1,14 +1,20 @@
-def caching_fibonacci():
-    cache: dict[int, int] = {}
+"""
+Завдання 5.1
+Створення функції для обрахунку ряду Фібоначчі, яка
+використовує кешування для спрощення обрахунків
+"""
 
-    def fibonacci(number: int) -> int:
-        if number <= 0:
+
+def caching_fibonacci():
+    # Створення словника для кешу
+    cache: dict[int, int] = {0: 0, 1: 1}  # додаємо виняткові значення 0 та 1
+
+    def fibonacci(number: int) -> int:  # Ф-ція Фібоначчі
+        if number < 0:  # Для значень менше 0
             return 0
-        elif number == 1:
-            return 1
-        elif number in cache:
-            return cache[number]
-        else:
+        elif number in cache:  # У випадку якщо було вже обчислення n
+            return cache[number]  # де n --> number
+        else:  # Якщо у кеші немає обчисленого n
             cache[number] = fibonacci(number - 1) + fibonacci(number - 2)
             return cache[number]
 
@@ -18,8 +24,8 @@ def caching_fibonacci():
 def main():
     fib = caching_fibonacci()
 
-    test_value = [1, 1, 5, 5, 10, 10, 20, 20]
-    for n in test_value:
+    test_value = [1, 1, 5, 5, 10, 10, 20, 20]  # Тестові значення
+    for n in test_value:  # Виводимо результат
         print(f"fib({n}) = {fib(n)}")
 
 
